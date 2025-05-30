10895c0664adc7356022963774c1d9531c380057
Brad Stricherz <Brad.Stricerz@gmail.com>
2025-05-30 17:09:39 -0500

    Changed port for docker

--- MapLarge/Program.cs
+++ MapLarge/Program.cs
@@ -1,6 +1,7 @@
 // Basic configuration and middleware pipeline for serving the API and SPA frontend
 
 // Configure the application builder and dependency injection
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Register controller support
@@ -9,12 +10,15 @@ builder.Services.AddControllers();
 builder.Services.Configure<FileBrowserOptions>(builder.Configuration.GetSection("FileBrowser"));
 
 // Enable CORS for cross-origin frontend access
-builder.Services.AddCors(options => {
+builder.Services.AddCors(options =>
+{
     options.AddDefaultPolicy(policy =>
         policy.AllowAnyOrigin()
             .AllowAnyHeader()
             .AllowAnyMethod());
 });
+
+builder.WebHost.UseUrls("http://*:8080");
 
 var app = builder.Build();
 
